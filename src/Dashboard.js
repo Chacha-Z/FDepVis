4bf8ffac923deeca01d99b3751f86f3cde8a7686
864690967@qq.com <86469097@qq.com>
2020-12-27 21:06:31 +0800

    自适应

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,10 @@
-import React, { Component } from 'react';
-import {  Row, Col  } from 'antd';
+import React, {
+  Component
+} from 'react';
+import {
+  Row,
+  Col
+} from 'antd';
 import ScatterPlot from './views/ScatterPlot';
 import FamilySelector from './views/FamilySelector';
 import FamilyTree from './views/FamilyTree';
@@ -10,41 +15,110 @@ import './dashboard.css';
 
 export default class Dashboard extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
 
-    render() {
-        return (
-            <div>
-                <Row style={{ height: '95vh' }}>
-                    <Col span={6} style={{ height: '100%' }}>
-                        <Row style={{ height: '40%' }}>
-                            <ScatterPlot />
-                        </Row>
-                        <Row style={{ height: '60%' }}>
-                            <FamilySelector />
-                        </Row>
-                    </Col>
-                    <Col span={18} style={{ height: '100%' }}>
-                        <Row style={{ height: '60%' }}>
-                            <FamilyTree/>
-                        </Row>
-                        <Row style={{ height: '40%' }}>
-                            <Col span={13} style={{ height: '100%' }}><AttributesHeatmap /></Col>
-                            <Col span={11} style={{ height: '100%' }}><DetailInfo /></Col>
-                        </Row>
-                    </Col>
-                </Row>
-                <Row style={{ height: '5vh' }} id='footer' >
-                    <div style={{textAlign: "center"}}>
-                        Source Code <a href='https://github.com/Chacha-Z/FDepVis'>https://github.com/Chacha-Z/FDepVis</a>;
-                        Author <a href='#'>我来取个名</a>;
-                    </div>
-                </Row>
-            </div>
-        )
-    }
+  render() {
+    return ( <
+      div >
+      <
+      Row style = {
+        {
+          height: '95vh'
+        }
+      } >
+      <
+      Col span = {
+        6
+      }
+      style = {
+        {
+          height: '100%'
+        }
+      } >
+      <
+      Row style = {
+        {
+          height: '40%'
+        }
+      } >
+      <
+      ScatterPlot / >
+      <
+      /Row> <
+      Row style = {
+        {
+          height: '60%'
+        }
+      } >
+      <
+      FamilySelector / >
+      <
+      /Row> <
+      /Col> <
+      Col span = {
+        18
+      }
+      style = {
+        {
+          height: '100%'
+        }
+      } >
+      <
+      Row style = {
+        {
+          height: '60%'
+        }
+      } >
+      <
+      FamilyTree / >
+      <
+      /Row> <
+      Row style = {
+        {
+          height: '40%'
+        }
+      } >
+      <
+      Col span = {
+        13
+      }
+      style = {
+        {
+          height: '100%',
+          backgroundColor: "#FFF"
+        }
+      } > < AttributesHeatmap / > < /Col> <
+      Col span = {
+        11
+      }
+      style = {
+        {
+          height: '100%'
+        }
+      } > < DetailInfo / > < /Col> <
+      /Row> <
+      /Col> <
+      /Row> <
+      Row style = {
+        {
+          height: '5vh'
+        }
+      }
+      id = 'footer' >
+      <
+      div style = {
+        {
+          textAlign: "center"
+        }
+      } >
+      Source Code < a href = 'https://github.com/Chacha-Z/FDepVis' > https: //github.com/Chacha-Z/FDepVis</a>;
+      Author < a href = '#' > 我来取个名 < /a>; <
+      /div> <
+      /Row> <
+      /div>
+    )
+  }
 }
